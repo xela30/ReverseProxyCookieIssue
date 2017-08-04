053cc2a13cc0ec032e07aeb4fb953a8c4719c284
Oleksandr Bulakaiev <Oleksandr_Bulakaiev@epam.com>
2017-08-04 14:52:07 +0300

    initial commit

---
+++ Service/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public string Get()
+        {
+            return this.Request.Headers["Cookie"].ToString();
+        }
+
+        [HttpGet]
+        [Route("set-cookie")]
+        public void SetCookie()
+        {
+            this.Response.Cookies.Append("my_cookie", "fake");
+        }
+    }
+}
